57746a2025b1a4d7e5c0288102b33c0e9e575b01
liugaoleng <3237976744@qq.com>
2025-11-20 19:20:57 +0800

    Dwd层-加购事实表开发完成

--- gmall2024-realtime-dev/realtime-dwd/realtime-dwd-trade-cart-add/src/main/java/com/atguigu/gmall/realtime/dwd/db/app/DwdTradeCartAdd.java
+++ gmall2024-realtime-dev/realtime-dwd/realtime-dwd-trade-cart-add/src/main/java/com/atguigu/gmall/realtime/dwd/db/app/DwdTradeCartAdd.java
@@ -13,6 +13,91 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
  */
 public class DwdTradeCartAdd extends BaseSQLApp {
     public static void main(String[] args) {
+        new DwdTradeCartAdd().start(10013, 4, Constant.TOPIC_DWD_TRADE_CART_ADD);
+    }
+
+
+
+    @Override
+    public void handle(StreamTableEnvironment tableEnv) {
+        //TODO 从kafka的topic_DB主题中读取业务数据   创建动态表
+        readOdsDb(tableEnv,Constant.TOPIC_DWD_TRADE_CART_ADD);
+
+        //TODO 过滤出加购数据   ---table = ‘cart_info’  type = ‘insert’ / type = ‘update’ 并且修改的字段是购买的数量同时修改后的数量大于修改之前的数量
+        Table cartInfo = tableEnv.sqlQuery("select \n" +
+                "   `data`['id'] id,\n" +
+                "   `data`['user_id'] user_id,\n" +
+                "   `data`['sku_id'] sku_id,\n" +
+                "   if(type='insert',`data`['sku_num'], CAST((CAST(data['sku_num'] AS INT) - CAST(`old`['sku_num'] AS INT)) AS STRING)) sku_num,\n" +
+                "   ts\n" +
+                "from topic_db \n" +
+                "where `table`='cart_info' \n" +
+                "and (\n" +
+                "    type = 'insert'\n" +
+                "    or\n" +
+                "    (type='update' and `old`['sku_num'] is not null and (CAST(data['sku_num'] AS INT) > CAST(`old`['sku_num'] AS INT)))\n" +
+                ")");
+
+        //TODO 将加购数据写到kafka主题中
+        //创建动态表和要写入kafka的主题进行映射
+        tableEnv.executeSql(" create table "+Constant.TOPIC_DWD_TRADE_CART_ADD+"(\n" +
+                "    id string,\n" +
+                "    user_id string,\n" +
+                "    sku_id string,\n" +
+                "    sku_num string,\n" +
+                "    ts bigint,\n" +
+                "    PRIMARY KEY (id) NOT ENFORCED\n" +
+                " )" + SQLUtil.getUpsertKafkaDDL(Constant.TOPIC_DWD_TRADE_CART_ADD));
+        //写入
+        cartInfo.executeInsert(Constant.TOPIC_DWD_TRADE_CART_ADD);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*public static void main(String[] args) {
         new DwdTradeCartAdd().start(
                 10013,
                 4,
@@ -52,5 +137,5 @@ public class DwdTradeCartAdd extends BaseSQLApp {
         //写入
         cartInfo.executeInsert(Constant.TOPIC_DWD_TRADE_CART_ADD);
 
-    }
+    }*/
 }
